cfebf1510df1f41d8cc6e012d0242b5905f808f4
Marc <mmichalewicz6@gmail.com>
2020-10-18 00:46:02 +0200

    interim

--- ibclient/resamplecsv.py
+++ ibclient/resamplecsv.py
@@ -1,15 +1,31 @@
 import os
 import pandas as pd
 
+import Misc.const
+
+mode = Misc.const.DOWNSPL1TO5
+
+if mode == Misc.const.DOWNSPL5TO12:
+    modulo = 12
+    orgdir = "5_min"
+elif mode == Misc.const.DOWNSPL1TO5:
+    modulo = 5
+    orgdir = "1_min"
+
 cols = ['date', 'open', 'high', 'low', 'close']
-paths = [ r"C:\git\ibpy\ibclient\data\STK_MIDPOINT\5_min", r"C:\git\ibpy\ibclient\data\OPT_MIDPOINT\5_min" ]
+paths = [ os.path.join(r"C:\git\ibpy\ibclient\data\STK_MIDPOINT",orgdir), os.path.join(r"C:\git\ibpy\ibclient\data\OPT_MIDPOINT", orgdir) ]
 for path in paths:
     os.chdir(path)
 
     newdir=""
     for p,n,f in os.walk(os.getcwd()):
         print(p)
-        newdir = p.replace("5_min", "60_min")
+
+        if mode == Misc.const.DOWNSPL5TO12:
+            newdir = p.replace("5_min", "60_min")
+        elif mode == Misc.const.DOWNSPL1TO5:
+            newdir = p.replace("1_min", "5_min")
+
         os.makedirs(newdir, exist_ok=True)
         for a in f:
             a = str(a)
@@ -21,7 +37,7 @@ for path in paths:
                 rowout=[]
                 i = 0
                 for index, row in dfin.iterrows():
-                    if index%12 == 0:
+                    if index%modulo == 0:
                         min = 500000
                         max = -500000
                         rowout.append(row['date'])
@@ -35,7 +51,7 @@ for path in paths:
                         if row[colname] > max:
                             max = row[colname]
 
-                    if index%12 == 11:
+                    if index%modulo == modulo-1:
                         rowout.append(max)
                         rowout.append(min)
                         rowout.append(row['close'])
